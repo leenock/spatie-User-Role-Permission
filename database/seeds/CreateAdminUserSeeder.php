a8f278c5f018d3381aa0f0d71da9ccbbc796a55f
leenock <leenock176@gmail.com>
2020-09-21 18:35:32 +0300

    first commit

---
+++ database/seeds/CreateAdminUserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use App\User;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class CreateAdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = User::create([
+            'name' => 'lee leenocks',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('admin')
+            ]);
+            $role = Role::create(['name' => 'Admin']);
+            $permissions = Permission::pluck('id','id')->all();
+            $role->syncPermissions($permissions);
+            $user->assignRole([$role->id]);
+    }
+}
